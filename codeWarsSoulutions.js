b0b89e585470be702bf7fff9f0aef643f34a306a
Johnny Vasil <jfvasil@gmail.com>
2022-07-25 11:42:08 -0400

    added class solutions

--- codeWarsSoulutions.js
+++ codeWarsSoulutions.js
@@ -218,5 +218,28 @@ function removeEveryOther(arr){
  })
   return newArr 
  } 
- 
+ //Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console. 
+function reverseParty(arr){
+    return arr.reverse()
+}
+console.log(reverseParty([1, 2, 3]))
+//Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+function squaresVsCubes(arrA, arrB){
+    return arrA.reduce((a,c) => a + Math.pow(c, 2)) > arrB.reduce((a, c) => a + Math.pow(c, 3))
+    
+}
+console.log(squaresVsCubes([2, 4, 6], [1, 2, 3]))
+//Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+// Some cases:
+// [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
+// [68, -1, 1, -7, 10, 10] => [-1, 10]
+function returnMulti(arr){
+    return arr.filter((x, i) => x % i == 0)
+}
+console.log(returnMulti([22, -6, 32, 82, 9, 25]))
+//Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.Return your answer as a number.
+function numSum(arr){
+    return arr.reduce((a,c) => a + Number(c),0)
+}
+console.log(numSum([2, '6', 5, '9']))
 
