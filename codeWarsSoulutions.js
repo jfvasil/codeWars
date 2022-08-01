44f7a3ecfb53a486ddaec46c556d3297b5caa838
Johnny Vasil <jfvasil@gmail.com>
2022-08-01 10:54:36 -0400

    New Solutions

--- codeWarsSoulutions.js
+++ codeWarsSoulutions.js
@@ -242,4 +242,19 @@ function numSum(arr){
     return arr.reduce((a,c) => a + Number(c),0)
 }
 console.log(numSum([2, '6', 5, '9']))
+// The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
 
+// To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+
+function openOrSenior(data){
+  let results = []
+  data.forEach(x => {
+    if(x[0] >= 55 && x[1] > 7){
+      results.push('Senior')
+    } else{
+      results.push('Open')
+    }
+  })
+  return results 
+  }
