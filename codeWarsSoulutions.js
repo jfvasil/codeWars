0d282938e1a84b88c167e7cbf659ffd22c4bb7cc
Johnny Vasil <jfvasil@gmail.com>
2022-08-08 12:23:18 -0400

    New solutions 08/08

--- codeWarsSoulutions.js
+++ codeWarsSoulutions.js
@@ -258,3 +258,22 @@ function openOrSenior(data){
   })
   return results 
   }
+  // There is a queue for the self-checkout tills at the supermarket. Your task is write a function to calculate the total time required for all the customers to check out!
+
+  // input
+  // customers: an array of positive integers representing the queue. Each integer represents a customer, and its value is the amount of time they require to check out.
+  // n: a positive integer, the number of checkout tills.
+  // output
+  // The function should return an integer, the total time required.
+
+  // Note that the bellow solution is not mine but copied here as a study help 
+  
+  function queueTime(customers, n) {
+  var w = new Array(n).fill(0);
+  for (let t of customers) {
+    let idx = w.indexOf(Math.min(...w));
+    w[idx] += t;
+  }
+  return Math.max(...w);
+}
+
