f6c71cd5fa67970fada613f56880c10b9eaa505a
Johnny Vasil <jfvasil@gmail.com>
2022-08-16 19:43:33 -0400

    new solutions 08/16

--- codeWarsSoulutions.js
+++ codeWarsSoulutions.js
@@ -307,3 +307,24 @@ function likes(names) {
       }
       return arr
     };
+    // Given a string of words, you need to find the highest scoring word.
+
+    // Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+    
+    // You need to return the highest scoring word as a string.
+    
+    // If two words score the same, return the word that appears earliest in the original string.
+    
+    // All letters will be lowercase and all inputs will be valid.
+
+    //this code is not mine and is here for study purposes
+    function high(s){
+      let as = s.split(' ').map(s=>[...s].reduce((a,b)=>a+b.charCodeAt(0)-96,0));
+      return s.split(' ')[as.indexOf(Math.max(...as))];
+    }
+    // Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+
+    function solution(nums){
+      return nums === null ? [] : nums.sort((a, b) => a - b)
+     }
