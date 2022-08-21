944b4f837ceb8fc303bec4c8e1f50a56363b361a
Johnny Vasil <jfvasil@gmail.com>
2022-08-21 10:25:51 -0400

    new solutions 08/21

--- codeWarsSoulutions.js
+++ codeWarsSoulutions.js
@@ -349,3 +349,34 @@ var isSquare = function(n){
   return Number.isInteger(square)
 
 }
+//Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+function replace(s){
+  return s.replace(/[aeiouAEIOU]/g, '!')
+}
+//Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+ 
+function move (position, roll) {
+  return position + roll * 2
+}
+//Create a function that accepts 2 string arguments and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+function strCount(str, letter){  
+  let sum = 0
+  let arr = str.split('')
+  arr.forEach(x => {
+    if(x === letter){
+      sum += 1
+    }
+  })
+  return sum 
+}
+//Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+function removeUrlAnchor(url){
+  let arr = url.split('')
+  if(arr.includes('#')){
+    let newArr = arr.slice(0, arr.indexOf('#'))
+    return newArr.join('')
+    } else{
+      return url
+    }
+}
