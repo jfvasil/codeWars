12cddd0639ad16d4b43f324f7ea0152c7b39617a
Johnny Vasil <jfvasil@gmail.com>
2022-08-27 20:54:25 -0400

    new solutions 08/27

--- codeWarsSoulutions.js
+++ codeWarsSoulutions.js
@@ -396,4 +396,29 @@ const areaOrPerimeter = function(l , w) {
     return (l +w) * 2
   }
 };
+//Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+function shortcut (string) {
+  return string.replace(/[aeiou]/g, '')
+  
+  }
+//The purpose of this kata is to work out just how many bottles of duty free whiskey you would have to buy such that the saving over the normal high street price would effectively cover the cost of your holiday.
 
+// You will be given the high street price (normPrice), the duty free discount (discount) and the cost of the holiday.
+
+// For example, if a bottle cost £10 normally and the discount in duty free was 10%, you would save £1 per bottle. If your holiday cost £500, the answer you should return would be 500.
+
+// All inputs will be integers. Please return an integer. Round down.
+
+
+  function dutyFree(normPrice, discount, hol){
+    let savings = normPrice *  (discount / 100)
+    let bottles = hol / savings
+    return Math.floor(bottles)
+    }
+//Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates).
+    function minValue(values){
+      let filtered = values.filter((x, i) => values.indexOf(x) === i)
+      let sorted = filtered.sort((a, b) => a - b)
+      let results = sorted.join('')
+      return Number(results)
+    }
