0e61654b4b104b00592fd5462fd3631b4f3b493c
Johnny Vasil <jfvasil@gmail.com>
2023-01-17 18:40:17 -0500

    new solutions 01/16/23

--- codeWarsSoulutions.js
+++ codeWarsSoulutions.js
@@ -591,3 +591,9 @@ function stray(numbers) {
   }
     return sum 
 }
+//Find the number with the most digits.
+
+//If two numbers in the argument array have the same number of digits, return the first one in the array.
+function findLongest(arr){
+  return arr.reduce((a, b) => (`${b}`.length > `${a}`.length) ? b : a)
+}
