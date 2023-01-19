a2b66846e0d15daeb88219adc62c50a68cce36fb
Johnny Vasil <jfvasil@gmail.com>
2023-01-19 11:22:08 -0500

    new solutions 01/19/23

--- codeWarsSoulutions.js
+++ codeWarsSoulutions.js
@@ -597,3 +597,19 @@ function stray(numbers) {
 function findLongest(arr){
   return arr.reduce((a, b) => (`${b}`.length > `${a}`.length) ? b : a)
 }
+
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+function count (string) {  
+  let obj = {}
+  for(let char of string){
+    if(!obj[char]){
+      obj[char] = 1
+    }else{
+      obj[char] += 1
+    }
+  }
+  return obj
+}
