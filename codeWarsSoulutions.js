7019e262e9664043c723dccb419a88f6f86b944a
Johnny Vasil <jfvasil@gmail.com>
2023-01-30 13:56:16 -0500

    new solutions 01/30/23

--- codeWarsSoulutions.js
+++ codeWarsSoulutions.js
@@ -793,3 +793,35 @@ function isPowerOfTwo(n){
  return num === 'true'
   
 }
+// 
+
+function evaporator(content, evap_per_day, threshold){ 
+  let days = 0
+   let conThresh = (content * threshold/100)
+   let result = content
+   while(result >= conThresh){
+     result -= result * evap_per_day/100
+     days++
+   }
+   return days
+  }
+
+  Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+// Rules for a smiling face:
+
+// Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+// A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+// Every smiling face must have a smiling mouth that should be marked with either ) or D
+// No additional characters are allowed except for those mentioned.
+
+function countSmileys(arr) {
+  let smiles = [':)', ':D', ':-)', ':-D', ':~)', ':~D', ';)', ';D', ';-)', ';-D', ';~)', ';~D']
+  let count = 0
+      for(let char of arr){
+        if(smiles.includes(char)){
+          count++
+        }
+      }
+    return count
+  }
