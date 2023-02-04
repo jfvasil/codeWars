bddb1c5b962a81844e4e765b84d5160fd941834b
Johnny Vasil <jfvasil@gmail.com>
2023-02-04 17:58:10 -0500

    new solutions 02/04/23

--- codeWarsSoulutions.js
+++ codeWarsSoulutions.js
@@ -870,3 +870,47 @@ function wordsToMarks(string){
     }
     return bumps > 15 ? 'Car Dead' : 'Woohoo!'
   }
+
+//   This kata is the first of a sequence of four about "Squared Strings".
+// You are given a string of n lines, each substring being n characters long: For example:
+// s = "abcd\nefgh\nijkl\nmnop"
+// We will study some transformations of this square of strings.
+// Vertical mirror: vert_mirror (or vertMirror or vert-mirror)
+// vert_mirror(s) => "dcba\nhgfe\nlkji\nponm"
+// Horizontal mirror: hor_mirror (or horMirror or hor-mirror)
+//  hor_mirror(s) => "mnop\nijkl\nefgh\nabcd"
+// Write these two functions
+// and
+// high-order function oper(fct, s) where
+// fct is the function of one variable f to apply to the string s (fct will be one of vertMirror, horMirror)
+
+function vertMirror(strng) {
+  return strng.split('\n').map(el => el.split('').reverse('').join('')).join('\n')
+}
+function horMirror(strng) {
+return strng.split('\n').reverse().join('\n')
+}
+function oper(fct, s) {
+ return fct(s)
+}
+// A stream of data is received and needs to be reversed.
+// Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
+// 11111111  00000000  00001111  10101010
+//  (byte1)   (byte2)   (byte3)   (byte4)
+// should become:
+// 10101010  00001111  00000000  11111111
+//  (byte4)   (byte3)   (byte2)   (byte1)
+// The total number of bits will always be a multiple of 8.
+// The data is given in an array as such:
+// [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
+
+function dataReverse(data) {
+  let segments = []
+  for(let i = 0; i<data.length; i+=8){
+    let chunk = data.slice(i, i + 8)
+    segments.push(chunk)
+  }
+  return segments.reverse().flat()
+}
+
+
