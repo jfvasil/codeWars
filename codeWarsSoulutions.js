d67eeacb9fa5c8ef4b947aa62ce658a6c5cb0a84
Johnny Vasil <jfvasil@gmail.com>
2023-02-20 13:50:29 -0500

    new solutions 02/20/23

--- codeWarsSoulutions.js
+++ codeWarsSoulutions.js
@@ -987,3 +987,16 @@ function meeting(s) {
   return arr
 
 }
+
+// Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+function twoSum(numbers, target) {
+    let indices = []
+    for(let i = 0; i<=numbers.length; i++){
+      for(let j = 1; j<=numbers.length; j++){
+        if(numbers[i] + numbers[j] === target && i !== j ){
+          indices = [i,j]
+        }
+      }
+    }
+  return indices
+}
