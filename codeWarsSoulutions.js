cfc0a01c1b4d1a60674ab550d35b35128bb58471
Johnny Vasil <jfvasil@gmail.com>
2023-02-21 16:55:58 -0500

    new solutions 02/21/23

--- codeWarsSoulutions.js
+++ codeWarsSoulutions.js
@@ -1000,3 +1000,23 @@ function twoSum(numbers, target) {
     }
   return indices
 }
+// Complete the function that takes two numbers as input, num and nth and return the nth digit of num (counting from right to left).
+
+// Note
+// If num is negative, ignore its sign and treat it as a positive value
+// If nth is not positive, return -1
+// Keep in mind that 42 = 00042. This means that findDigit(42, 5) would return 0
+
+
+var findDigit = function(num, nth){
+   num = Math.abs(num)
+  if(nth <= 0){
+    return -1 
+  } else if(nth > num.toString().length){
+    return 0 
+  } else{
+ 
+    let arr = num.toString().split('').reverse().map(el => Number(el))
+    return arr[nth - 1]
+   }
+}
