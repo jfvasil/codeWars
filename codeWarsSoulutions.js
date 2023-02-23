b4e3920051375ff6ca7190e90e3f7b48fd7a00a3
Johnny Vasil <jfvasil@gmail.com>
2023-02-23 16:43:45 -0500

    new solution 02/23/23

--- codeWarsSoulutions.js
+++ codeWarsSoulutions.js
@@ -1020,3 +1020,11 @@ var findDigit = function(num, nth){
     return arr[nth - 1]
    }
 }
+// Write a function that takes a positive integer n, sums all the cubed values from 1 to n (inclusive), and returns that sum.
+function sumCubes(n){
+  let sum = 0
+  for(let i = 1; i<=n; i++){
+    sum += Math.pow(i, 3)
+  }
+  return sum
+}
