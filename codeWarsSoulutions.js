5c193b2071fa10c2450f4c016b36d5a2b3bd7b9e
Johnny Vasil <jfvasil@gmail.com>
2023-03-29 10:55:16 -0400

    new solution 03/29/23

--- codeWarsSoulutions.js
+++ codeWarsSoulutions.js
@@ -1217,3 +1217,9 @@ reverse = function(array) {
   }
   return fin
 }
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+
+function digitize(n) {
+  return n.toString().split('').reverse().map(el => Number(el))
+}
