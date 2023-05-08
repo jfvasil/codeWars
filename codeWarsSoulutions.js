888e9aad9e221421de02e3749b3fa5a7b8556a13
Johnny Vasil <jfvasil@gmail.com>
2023-05-08 13:18:21 -0400

    new solutions 05/08/23

--- codeWarsSoulutions.js
+++ codeWarsSoulutions.js
@@ -1260,3 +1260,20 @@ function divCon(x){
   })
   return sumN - sumS
   }
+
+
+//   Write a function to get the first element(s) of a sequence. Passing a parameter n (default=1) will return the first n element(s) of the sequence.
+
+// If n == 0 return an empty sequence []
+
+function first(arr, n) {
+  if(n === 0){
+    return []
+  }
+  
+  if(!n){
+    return [arr[0]]
+  } else{
+    return arr.slice(0,n)
+  }
+}
